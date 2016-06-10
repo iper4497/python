46a6aa5592286f1adffac0c4cc3efe6dab7cf858
pensar <eqzqcj@pgpmail.cc>
2016-06-10 16:59:58 +0200

    mas-lib

---
+++ python-para-leer/lib/systema/archivos/comprovar.py
@@ -0,0 +1,21 @@
+def lib(a, b, v): 
+    b = open(b, 'r')
+    v = open(v, 'r') 
+    a[3] = -1
+    for a[2] in range(a[1]):
+        if b.readline() == v.readline():
+            a[3] = a[3] + 1
+    v.close()
+    b.close()
+    if a[3] == a[2]:
+        a[3] = True
+    return(a[3], a[2])
+class comprovar():
+    
+    def __init__(self, b, v):
+        a = range(30)
+        a[1] = 200
+        #b = 'archivo2'
+        # v = 'archivo1'
+        a = lib(a, b, v)
+        return(a)
---
+++ python-para-leer/lib/systema/archivos/usarcomprovar.py
@@ -0,0 +1,3 @@
+import comprovar
+a = comprovar.comprovar('arcvhivo1', 'archivo2')
+print(a)
