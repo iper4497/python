cecb3b4784e9132bb5c20c1d23f2a459087e5759
pensar <eqzqcj@pgpmail.cc>
2016-07-08 18:00:53 +0200

    Arreglar-librerias

--- lib/prueva/lector_de_libros/lectorlib.py
+++ lib/prueva/lector_de_libros/lectorlib.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
-class lector(a):
+import command
+class lector(object):
     def __init__(self, a):
         self.a = '/tmp/pdf'
-        os.system('pdftotext' + a + self.a)
+        self.pal = command.system('pdftotext' + a + self.a)
     def archivo(self, a):
-        #guardar en archvio
+        os.system('espeak' + self.a + '-w ' + a)
     def escuchar(self):
-        #Escuchar en vivo
-        
+        for a in self.pal:
+            os.system('espeak' + a)
